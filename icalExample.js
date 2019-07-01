ca128193274de4f3adc32eebe2fc6fef203610de
Vijay Das <vijaytdas@gmail.com>
2019-07-01 17:54:00 -0500

    add iCalExample

---
+++ icalExample.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const ical = require("ical");
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec"
+];
+
+ical.fromURL(
+  "https://www.airbnb.com/calendar/ical/10149981.ics?s=975ab57d648fcfb3b5132fb5a657adea",
+  {},
+  function(err, data) {
+    for (let k in data) {
+      if (data.hasOwnProperty(k)) {
+        var ev = data[k];
+        if (data[k].type == "VEVENT") {
+          console.log(
+            ev.description
+          );
+        }
+      }
+    }
+  }
+);
+
